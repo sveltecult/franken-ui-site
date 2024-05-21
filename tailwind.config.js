0289da89bdb25a79ecb3a24a477f20590bfb2d9e
Sveltecult <sveltecult@proton.me>
2024-05-21 09:56:49 +0800

    add about and changelog pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,7 @@ import presetQuick from "franken-ui/shadcn-ui/preset-quick";
 /** @type {import('tailwindcss').Config} */
 export default {
   presets: [presetQuick()],
-  content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "./md/**/*.md",
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     container: {
       center: true,
